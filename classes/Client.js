77837c9789f1b77a9dab4279adae6002a37bcd05
unknown <odionedwards@gmail.com>
2019-06-15 12:34:04 -0400

    added new folder "classes" and created a file for each class that will make up TasksJS

---
+++ classes/Client.js
@@ -0,0 +1,30 @@
+//The purpose of the client class is to have just one abstraction for sending request
+//that will be used by all other classes. Even if I decide to change what module i use
+//to actually send the request, the abstraction remains the same
+import httpClient from "../request";
+
+class client {
+  request({ method, url, body, json }, cb) {
+    httpClient({ method, url, body, json }, (err, res, body) => {
+      if (err) {
+        if (typeof cb === "function") cb(err);
+      } else if (res.statusCode >= 400) {
+        if (typeof cb === "function") cb(body);
+      } else {
+        if (typeof cb === "function") cb(null, body, res);
+      }
+    });
+  }
+
+  uploadFile({ url, formData, json }, cb) {
+    httpClient.post({ url, formData, json }, (err, res, body) => {
+      if (err) {
+        if (typeof cb === "function") cb(err);
+      } else if (res.statusCode >= 400) {
+        if (typeof cb === "function") cb(body);
+      } else {
+        if (typeof cb === "function") cb(null, body);
+      }
+    });
+  }
+}
