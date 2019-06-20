963d1c2724805ae3debcaa4efe55c8f52a98f2ef
unknown <odionedwards@gmail.com>
2019-06-20 03:28:55 -0400

    refactored serverModule into its own file. Updated how serverMod configurations are handled. still needs testing

--- classes/ServerModule.js
+++ classes/ServerModule.js
@@ -1,16 +1,16 @@
 const TasksJSModule = require("./Module");
 const Server = require("./Server");
 const shortid = require("shortid");
-function ServerModule({ name, app, modConstructor, server }, config) {
+function ServerModule({ name, app, modConstructor, server }, cfCallback) {
   //serverMod is inheriting from TasksJSModule using this weird pattern. Lol
   let serverMod = new TasksJSModule.apply(this, [name, app, modConstructor]);
   //This sets up a socket.io namespace for this ServerMod
   let nameSpace = shortid();
-  let nsp = server.io.of("/" + nameSpace);
+  let nsp = server.io.of(`/${nameSpace}`);
 
   serverMod.name = name;
   serverMod.nsp = nsp;
-
+  //here we're using the socket.io namespace to fire an event called dispatch
   serverMod.emit = (name, data) => {
     nsp.emit("dispatch", {
       id: shortid(),
@@ -20,74 +20,76 @@ function ServerModule({ name, app, modConstructor, server }, config) {
       sent_at: Date()
     });
   };
+  //using modConstructor.apply let's us determine that the this object will be this serveModule
+  modConstructor.apply(serverMod, []);
+  //configuration options determine how routing is handled
+  let options = configurationHandler(serverMod, app, cfCallback);
+  Server.addRoute(name, serverMod, options, nameSpace);
 }
 
-function serverModFactory(mod) {
-  var thisMod = {},
-    config_all = {},
-    config_options = {};
-
-  thisMod._class = "serverMod";
-  thisMod._name = mod.name;
-
-  thisMod.config = config;
-  thisMod.configAll = configAll;
-
-  thisMod.useModule = useModule;
-  thisMod.useService = useService;
-
-  function config(name, options) {
-    config_options[name] = options;
-  }
-
-  function configAll(options) {
-    config_all = options;
-  }
-
-  var _nsp = randomStr();
-
-  var nsp = _server.io.of("/" + _nsp);
-
-  thisMod.nsp = nsp;
-
-  thisMod.emit = function(eventName, data) {
-    nsp.emit("dispatch", {
-      id: randomStr(10),
-      name: eventName,
-      data: data,
-      sent_by: appName + "." + mod.name,
-      sent_at: Date()
-    });
-  };
-
-  mod.modConstructor.apply(thisMod, []);
-  //loop through all properties on serverMod
-  obj(thisMod).forEach(function(value, pName) {
+//configurationHandler creates an object that has methods that are used
+//to set configuration options. This object is passed to the configuration
+//callback which is the second parameter of the app.serverMod method
+function configurationHandler(serverMod, app, cfCallback) {
+  const handler = {};
+  const configOptions = {};
+  const props = Object.getOwnPropertyNames(serverMod);
+  const reservedProps = ["emit", "useModule", "useService"];
+  //loop through each property on the serverMod that is a function
+  //in order to create a handler that can set configOptions for each method
+  props.forEach(prop => {
     if (
-      [
-        "config",
-        "configAll",
-        "handle",
-        "emit",
-        "useModule",
-        "useService"
-      ].indexOf(pName) === -1 &&
+      //exclude serverMod original methods
+      reservedProps.indexOf(pName) === -1 &&
       typeof thisMod[pName] === "function"
     ) {
-      config_options[pName] = config_options[pName] || {};
+      handler[prop] = configurator(prop);
 
-      //loop through all config options on config_all object
-      obj(config_all).forEach(function(opt) {
-        //apply config_all options for each serverMod method where config_options have not already been set
-        config_options[pName][opt] =
-          config_options[pName][opt] || config_all[opt];
-      });
-
-      //all request to serverMod are PUTs by default
-      config_options[pName].request_method =
-        config_options[pName].request_method || "PUT";
+      configOptions[prop] = {
+        method: "PUT",
+        route: null
+      };
     }
   });
+  //using persistance here so that the configurator will know
+  //what the property it's supposed to set
+  const configurator = fnName => {
+    const setter = {
+      setRoute: value => setOption("route", value),
+      setMethod: value => setOption("request_method", value),
+      inferRoute: () =>
+        setOption("route", `${app.route}/${serverMod.name}/${fnName}`)
+    };
+    const setOption = (optionName, value) => {
+      configOptions[fnName][optionName] = value;
+      //setter returned here to allow chaining
+      return setter;
+    };
+    return setter;
+  };
 
-  _server.addRoute(mod.name, thisMod, config_options, _nsp);
+  //the following functions are for configuring all the methods of
+  //the serverMod at once
+  handler.setNamespace = value => {
+    configOptions.nsp = value;
+    return handler;
+  };
+  handler.setRoutes = value => {
+    props.forEach(prop => handler[prop].setRoute(`${value}/${prop}`));
+    return handler;
+  };
+  handler.inferRoutes = () => {
+    props.forEach(prop => handler[prop].inferRoute());
+    return handler;
+  };
+
+  handler.setMethods = value => {
+    props.forEach(prop => handler[prop].setMethod(value));
+    return handler;
+  };
+
+  //inside the cfCallback serverMod configuration should look like this:
+  //example: handler.propertyName.setMeothod('GET').inferRoute()
+  if (typeof cfCallback === "function") cfCallback(handler);
+  return configOptions;
 }
