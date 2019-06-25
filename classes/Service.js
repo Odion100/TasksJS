23d7d10e70505238ecc65ad010a70840233f63e2
unknown <odionedwards@gmail.com>
2019-06-25 18:22:30 -0400

    requestHandler reconnect logic worked out

--- classes/Service.js
+++ classes/Service.js
@@ -33,8 +33,8 @@ const serverModuleRequestHandler = (
   //for each method on the serverMod, requestHandler returns a function that will handle requests
   methods.forEach(method => (serverMod[method.name] = requestHandler(method)));
 
+  //this method sets up the url to make request to the backend module
   serverMod.__setConnection = (host, route, port) => {
-    //these are the routes to the serverMod in the backend
     const singleFileURL = `http://${host}:${port}/sf/${route}`;
     const multiFileURL = `http://${host}:${port}/mf/${route}`;
     const url = `http://${host}:${port}${route}`;
@@ -43,11 +43,37 @@ const serverModuleRequestHandler = (
 
   const requestHandler = ({ method, name }) => {
     //create the request url using the serverMod and method data
-    return (data, cb, errCount) => {
-      const requestCB = (err, results) => {
+    const fn = (data, cb, errCount = 0) => {
+      //if there is a file or files property on the data object make the
+      //request to the appropriate file upload route
+      switch (true) {
+        case data.file:
+          return Client.uploadFile(
+            { url: `${singleFileURL}/${name}`, method, data },
+            callBack
+          );
+        case data.files:
+          return Client.uploadFile(
+            { url: `${multiFileURL}/${name}`, method, data },
+            callBack
+          );
+        default:
+          return Client.request(
+            { url: `${url}/${name}`, method, data },
+            callBack
+          );
+      }
+      //handles callback after each request
+      const callBack = (err, results) => {
         if (err) {
           if (err.invalidMapERROR) {
-            invalidRouteERROR(err, () => {});
+            //throw an error if a request fails three times in a row
+            if (errCount >= 3)
+              throw Error(
+                "(TasksJS): Invalid route. Failed to reconnect after 3 attempts."
+              );
+            //reset the connection then try to make the same request again
+            resetConnection(err.maps, service, () => fn(data, cb, errCount++));
           } else {
             if (typeof cb === "function") cb(err);
           }
@@ -55,91 +81,19 @@ const serverModuleRequestHandler = (
           if (typeof cb === "function") cb(null, results);
         }
       };
-
-      //if there is a file or files property on the data object make the
-      //request to the appropriate file upload route
-      switch (true) {
-        case data.file:
-          return Client.uploadFile(
-            { url: `${singleFileURL}/${name}`, method, data },
-            requestCB
-          );
-        case data.files:
-          return Client.uploadFile(
-            { url: `${multiFileURL}/${name}`, method, data },
-            requestCB
-          );
-        default:
-          return Client.request(
-            { url: `${url}/${name}`, method, data },
-            requestCB
-          );
-      }
     };
+
+    return fn;
   };
 };
 const connectWebSocket = () => {};
 //Use the maps from error object to the path to each ServerModule in the service
-const requestErrHandler = (
-  { maps },
-  service,
-  modName,
-  methodName,
-  errCount
-) => {
-  //throw an error if a request fails three times in a row
-  if (errCount >= 3)
-    throw Error(
-      "(TasksJS): Invalid route. Failed to reconnect after 3 attempts."
-    );
+const resetConnection = (maps, service, cb) => {
   //instead of re-instantiating the backend serverModule we use the ___setConnection
   //method to update the serverModules' connection data
   maps.forEach(map =>
     service[map.modName].__setConnection(map.host, map.port, map.route)
   );
 
-  service[modName][methodName];
-  3;
+  cb();
 };
-const invalidRouteERROR = () => {};
-
-var attempts = 0;
-function mapErrHandler(new_api, req, callBack, handler) {
-  //continually attempt to make the request to a new service api
-  attempts++;
-  if (attempts >= 3) {
-    throw "tasksJS ERROR: Invalid Map!!! FAILED TO CONNECT TO APP AFTER " +
-      attempts +
-      " ATTEMPTS!!!";
-  } else {
-    var new_maps = new_api.maps;
-
-    //use new_maps to update the path for each serverMod of this service
-    for (var i = 0; i < new_maps.length; i++) {
-      var new_route = new_maps[i].route.join("/");
-      //loop throuhg each serverMod in the service and use _updatePath method to update the route to the serverMod
-      services[serviceName].service[new_maps[i].modName]._updatePath(
-        new_route,
-        new_api.host,
-        new_maps[i].nsp
-      );
-    }
-
-    if (typeof services[serviceName].onLoad === "function") {
-      //run onLoad Handler first
-      services[serviceName].onLoad(function() {
-        //use this handle on reqHandler to resend the request
-        handler.run(req.data, function(err, data) {
-          callBack(err, data);
-          attempts = 0;
-        });
-      });
-    } else {
-      //use this handle on reqHandler to resend the request
-      handler.run(req.data, function(err, data) {
-        callBack(err, data);
-        attempts = 0;
-      });
-    }
-  }
-}
