4d7bf71501d7764a5663fbced32e1eec9af7d5c3
unknown <odionedwards@gmail.com>
2019-08-25 17:46:34 -0400

    TasksJSModule unit test outline complete

--- test/test.js
+++ test/test.js
@@ -2,10 +2,9 @@ const { expect } = require("chai");
 const TasksJSModule = require("../tjs/Module");
 
 describe("TasksJSModule Tests", function() {
-  describe("Create a TasksJSModule instance with all parameters as null", () => {
+  describe("Create a TasksJSModule instance with all parameters as null: TasksJSModule()", () => {
     it("Should return a TaskJSModule instance with all basic properties and methods", () => {
-      const mod = TasksJSModule();
-      expect(mod)
+      expect(TasksJSModule())
         .to.be.an("object")
         .that.has.all.keys(
           "on",
@@ -21,5 +20,14 @@ describe("TasksJSModule Tests", function() {
         .that.respondsTo("useService")
         .that.respondsTo("useConfig");
     });
+
+    it("Should be able to emit and handle events", () => {});
+
+    it("should return null when attempting to retrieve systemObjects: (i.e., TasksJSModules, ServerModules, and Services)", () => {});
+  });
+
+  describe("Create a TasksJSModule instance with all parameters: TasksJSModules(name, constructor, systemObjects)", () => {
+    it("should have extra methods and properties set inside the constructor function", () => {});
+    it("should be able to retrieve systemObjects: : (i.e., TasksJSModules, ServerModules, and Services)", () => {});
   });
 });
