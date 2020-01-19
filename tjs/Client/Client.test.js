120b1ef92b76e007de5f8bc8e63e2023dbd38515
Odion100 <odionedwards@gmail.com>
2020-01-18 19:58:51 -0500

    updating test

--- tjs/Client/Client.test.js
+++ tjs/Client/Client.test.js
@@ -6,75 +6,8 @@ const fs = require("fs");
 const { TasksJSClient } = require("../../index")();
 const port = 4789;
 
-const ExpressServerSetup = () => {
-  //express server
-  const express = require("express");
-  const server = express();
-  //express middleware
-  const bodyParser = require("body-parser");
-  const multer = require("multer");
-  //express middleware setup
-  const TEMP_LOCATION = "./temp";
-  const mime = require("mime");
-  const shortId = require("shortid");
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, TEMP_LOCATION),
-    filename: (req, file, cb) =>
-      cb(null, `${shortId()}.${mime.getExtension(file.mimetype)}`)
-  });
-
-  //middleware functions
-  const sf = multer({ storage: storage }).single("file");
-  const mf = multer({ storage: storage }).array("files");
-  //the sf and mf functions are used to a extract file from the req during a file upload
-  //a property named file and files will be added to the req object respectively
-  const singleFileUpload = (req, res, next) =>
-    sf(req, res, err => {
-      if (err) res.json(errorResponseBuilder(err));
-      else next();
-    });
-
-  const multiFileUpload = (req, res, next) =>
-    mf(req, res, err => {
-      if (err) res.json(errorResponseBuilder(err));
-      else next();
-    });
-
-  const response = (req, res) => {
-    const { body, method } = req;
-    body.testPassed = true;
-    res.json({ method, ...body });
-  };
-
-  const uploadResponse = (req, res) => {
-    const { file } = req;
-    const json = JSON.parse(fs.readFileSync(file.path));
-
-    res.json({ file, ...json });
-  };
-
-  const multiUploadResponse = (req, res) => {
-    const { files } = req;
-    const json = JSON.parse(fs.readFileSync(files[0].path));
-
-    res.json({ files, ...json });
-  };
-
-  server.use("/sf", singleFileUpload);
-  server.use("/mf", multiFileUpload);
-  server.use(bodyParser.json({ limit: "5mb" }));
-
-  server.get("/test", response);
-  server.put("/test", response);
-  server.post("/test", response);
-  server.delete("/test", response);
-  server.post("/sf/test", uploadResponse);
-  server.post("/mf/test", multiUploadResponse);
-  server.listen(port, console.log(`(TestServer) listening on port:${port}`));
-
-  return server;
-};
-ExpressServerSetup();
+//test server setup
+require("./test.server")(port);
 
 describe("TasksJSClient Test", () => {
   const Client = TasksJSClient();
@@ -90,20 +23,31 @@ describe("TasksJSClient Test", () => {
       .that.respondsTo("upload");
   });
 
-  it("should be able to make (get) requests using a promise or callback", () => {
-    const getWithPromise = Client.request({ method: "GET", url });
+  it("should be able to make http requests using a callback", () => {
     const getWithCallback = new Promise((resolve, reject) => {
-      Client.request({ method: "GET", url }),
+      console.log("kdlskls");
+      Client.request({ method: "GET", url, body: { getWithCallback: true } }),
         (err, results) => {
           if (err) reject(err);
           else resolve(results);
+          console.log(results);
         };
     });
+
+    expect(getWithCallback).eventually.to.be.an("Objdddddect");
+  });
+
+  /*   it("should be able to make http requests using a promise", () => {
+    const getWithPromise = Client.request({
+      method: "GET",
+      url,
+      body: { getWithPromise: true }
+    });
+
     expect(getWithPromise)
       .to.eventually.be.an("Object")
-      .that.has.property("method", "GET");
-    expect(getWithCallback)
-      .to.eventually.be.an("Object")
-      .that.has.property("method", "GET");
-  });
+      .that.has.property("method", "GET")
+      .that.has.property("testPassed", true)
+      .that.has.property("getWithPromise", true);
+  }); */
 });
---
+++ tjs/Client/test.server.js
@@ -0,0 +1,68 @@
+module.exports = TestServerSetup = port => {
+  //express server
+  const express = require("express");
+  const server = express();
+  //express middleware
+  const bodyParser = require("body-parser");
+  const multer = require("multer");
+  //express middleware setup
+  const TEMP_LOCATION = "./temp";
+  const mime = require("mime");
+  const shortId = require("shortid");
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, TEMP_LOCATION),
+    filename: (req, file, cb) =>
+      cb(null, `${shortId()}.${mime.getExtension(file.mimetype)}`)
+  });
+
+  //middleware functions
+  const sf = multer({ storage: storage }).single("file");
+  const mf = multer({ storage: storage }).array("files");
+  //the sf and mf functions are used to a extract file from the req during a file upload
+  //a property named file and files will be added to the req object respectively
+  const singleFileUpload = (req, res, next) =>
+    sf(req, res, err => {
+      if (err) res.json(errorResponseBuilder(err));
+      else next();
+    });
+
+  const multiFileUpload = (req, res, next) =>
+    mf(req, res, err => {
+      if (err) res.json(errorResponseBuilder(err));
+      else next();
+    });
+
+  const response = (req, res) => {
+    const { body, method } = req;
+    body.testPassed = true;
+    res.json({ method, ...body });
+  };
+
+  const uploadResponse = (req, res) => {
+    const { file } = req;
+    const json = JSON.parse(fs.readFileSync(file.path));
+
+    res.json({ file, ...json });
+  };
+
+  const multiUploadResponse = (req, res) => {
+    const { files } = req;
+    const json = JSON.parse(fs.readFileSync(files[0].path));
+
+    res.json({ files, ...json });
+  };
+
+  server.use("/sf", singleFileUpload);
+  server.use("/mf", multiFileUpload);
+  server.use(bodyParser.json({ limit: "5mb" }));
+
+  server.get("/test", response);
+  server.put("/test", response);
+  server.post("/test", response);
+  server.delete("/test", response);
+  server.post("/sf/test", uploadResponse);
+  server.post("/mf/test", multiUploadResponse);
+  server.listen(port, console.log(`(TestServer) listening on port:${port}`));
+
+  return server;
+};
