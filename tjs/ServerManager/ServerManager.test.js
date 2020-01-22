97f5109152aa56bfe3ece39bb6a0b301478bde89
odion100 <odionedwards@gmail.com>
2020-01-21 21:48:51 -0500

    ServerManager test setup;

---
+++ tjs/ServerManager/ServerManager.test.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+const TasksJSServerManager = require("./ServerManager");
+describe("ServerManager", () => {
+  const ServerManager = TasksJSServerManager();
+
+  it("should return an TasksJSServerManager instance", () => {});
+
+  it("should accept GET request for ServerModule Connection Data on the given route", () => {});
+
+  it("should be able to added ServerModule data to the ServerManager instance that can be accessed via a GET request", () => {});
+});
