5d381764e8d3b0dd5493f6dcbdeaefb48694ddbd
odion100 <odionedwards@gmail.com>
2020-03-04 17:10:20 -0500

    added App.startService test

--- tjs/App/tests/App.test.js
+++ tjs/App/tests/App.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const AppFactory = require("../App");
-//should be an tasksjs app
+const HttpClient = require("../../HttpClient/HttpClient")();
 describe("App Factory", () => {
   it("should return a TasksJS App", () => {
     const App = AppFactory();
@@ -29,8 +29,8 @@ describe("App Factory", () => {
 });
 
 describe("App", () => {
-  const App = AppFactory();
-  it("should be able to initialize a module", async () => {
+  it("should be able to use App.module to initialize a module", async () => {
+    const App = AppFactory();
     return new Promise(resolve =>
       App.module("test", function() {
         expect(this)
@@ -54,8 +54,34 @@ describe("App", () => {
       })
     );
   });
+  it("should be able to use App.startService to start as TasksJSService", async () => {
+    const App = AppFactory();
+    const route = "test-service";
+    const port = "8493";
+    const url = `http://localhost:${port}/${route}`;
+    App.startService({ route, port });
+
+    const connData = await HttpClient.request({ url });
+
+    expect(connData)
+      .to.be.an("Object")
+      .that.has.all.keys(
+        "TasksJSService",
+        "host",
+        "port",
+        "modules",
+        "route",
+        "namespace",
+        "serviceUrl"
+      )
+      .that.has.property("modules")
+      .that.is.an("array").that.is.empty;
+    expect(connData.serviceUrl).to.equal(url);
+  });
 });
-//should be able to initialize modules
+describe("App LifeCycle Events", () => {
+  it('should be able to use App.on("init_complete") to catch this event', () => {});
+});
 
 //should be able to load and use services with the app scope
 // should be able to use onLoad when loading a service
